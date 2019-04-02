fe7eb3a0aa2b34f1d5d15b196a3a3405df952e19
syrine08 <49120348+syrine08@users.noreply.github.com>
2019-04-02 11:00:44 +0200

    Add files via upload

---
+++ frontend/entite/wish.php
@@ -0,0 +1,92 @@
+<?php
+class Wish
+{
+	private $Reference;
+	private $Nom;
+	private $Prix;
+	private $Description;
+	private $Photo;
+	private $Cathegory;
+	private $mail
+	
+
+	 function __construct($reference,$nom,$prix,$description,$cathegory,$photo){
+
+	 	$this->Reference = $reference;
+	 	$this->Nom = $nom;
+	 	$this->Prix=$prix;
+	 	$this->Description=$description;
+	 	$this->Cathegory=$cathegory; 
+	 	$this->Photo=$photo; 
+	 	
+
+	 }
+
+	 function getReference()
+	 {
+	 	return $this->Reference;
+	 }
+
+	 function getNom()
+	 {
+	 	return $this->Nom;
+	 }
+
+	 function getPrix()
+	 {
+	 	return $this->Prix;
+	 }
+
+	 function getDescription()
+	 {
+	 	return $this->Description;
+	 }
+
+	 function getPhoto()
+	 {
+	 	return $this->Photo;
+	 }
+
+	 function getCathegory()
+	 {
+	 	return $this->Cathegory;
+	 }
+	 function setReference($reference)
+	 {
+	 	$this->Reference = $reference;
+	 } function getmail()
+	 {
+	 	return $this->mail;
+	 }
+	 function setmail($mail)
+	 {
+	 	$this->mai = $mail;
+	 }
+
+	 function setNom($nom)
+	 {
+	 	$this->Nom = $nom;
+	 }
+
+	 function setPrix($prix)
+	 {
+	 	$this->Prix = $prix;
+	 }
+
+	 function setDescription($description)
+	 {
+	 	$this->Description = $description;
+	 }
+	 function setCathegory()
+	 {
+	 	 $this->Cathegory = $cathegory;
+	 }
+	 function setPhoto($photo)
+	 {
+	 	$this->Photo = $photo;
+	 }
+
+	
+}
+
+?>
