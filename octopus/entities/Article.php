79723e790e7497d771bc482002b619abd9f630b3
chadiznina <49171544+chadiznina@users.noreply.github.com>
2019-04-30 02:41:02 +0100

    Add files via upload

--- octopus/entities/Article.php
+++ octopus/entities/Article.php
@@ -1,22 +1,31 @@
 <?php
 
 class Article
-{
+{     private $name;
       private $avatar;
       private $description;
       private $type;
+      private $image;
 
-  function __construct($avatar,$description,$type)
+  function __construct($name,$avatar,$description,$type,$image)
   {
+    $this->name = $name;
     $this->avatar = $avatar;
     $this->type = $type;
     $this->description = $description;
+    $this->image = $image;
   }
+  function getname (){return $this->name;}
   function getavatar (){return $this->avatar;}
   function gettype (){return $this->type;}
   function getdescription (){return $this->description;}
-  
-
+  function getimage (){return $this->image;}
+  function setname($name){
+    $this->name=$name;
+  }
+function setimage($image){
+    $this->image=$image;
+  }
   function setavatar($avatar){
     $this->avatar=$avatar;
   }
